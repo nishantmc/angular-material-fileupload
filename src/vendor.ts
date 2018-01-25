1d12c4cc557487827ac2bac0fc59512a25e4c515
Nishant M C <nchitham@cisco.com>
2018-01-24 22:51:16 -0600

    0.0.8 * Removed unwanted deps * Update material to stable version

--- src/vendor.ts
+++ src/vendor.ts
@@ -3,8 +3,7 @@ import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
-import '@angular/http';
-import '@angular/router';
+
 import '@angular/material'
 import '@angular/material/prebuilt-themes/indigo-pink.css';
 
