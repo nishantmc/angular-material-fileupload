1d6cde9c378809367fb9fa21889c93039cb27237
Nishant M C <nchitham@cisco.com>
2018-04-01 22:51:44 -0500

    Fix support of custom HTTP headers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Input, Component } from '@angular/core';
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from '@angular/common/http';
+
 
 import '../assets/css/styles.css';
 
@@ -8,6 +10,16 @@ import '../assets/css/styles.css';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @Input()
+  httpRequestHeaders: HttpHeaders | {
+    [header: string]: string | string[];
+  } = new HttpHeaders().set("sampleHeader", "headerValue").set("sampleHeader1", "headerValue1");
+
+  @Input()
+  httpRequestParams: HttpParams | {
+    [param: string]: string | string[];
+  } = new HttpParams().set("sampleRequestParam", "requestValue").set("sampleRequestParam1", "requestValue1");
 
   public uploadEvent($event: any) {
     console.log('from client' + JSON.stringify($event));
