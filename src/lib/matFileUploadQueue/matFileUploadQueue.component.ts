c60e3c3dacacfc4c26cd5386e8bacf8c0ceb74b0
Miroslav-MAC18 <miroslav@Miroslav-MAC18s-MacBook-Pro.local>
2018-11-19 10:21:40 +0100

    removeAll
    emits remove event for all items present in the queue.

--- src/lib/matFileUploadQueue/matFileUploadQueue.component.ts
+++ src/lib/matFileUploadQueue/matFileUploadQueue.component.ts
@@ -10,76 +10,77 @@ import { HttpHeaders, HttpParams } from '@angular/common/http';
  * A material design file upload queue component.
  */
 @Component({
-    selector: 'mat-file-upload-queue',
-    templateUrl: `matFileUploadQueue.component.html`,
-    exportAs: 'matFileUploadQueue',
-  })
-  export class MatFileUploadQueue implements OnDestroy {
+  selector: 'mat-file-upload-queue',
+  templateUrl: `matFileUploadQueue.component.html`,
+  exportAs: 'matFileUploadQueue',
+})
+export class MatFileUploadQueue implements OnDestroy {
 
-    @ContentChildren(forwardRef(() => MatFileUpload)) fileUploads: QueryList<MatFileUpload>;
+  @ContentChildren(forwardRef(() => MatFileUpload)) fileUploads: QueryList<MatFileUpload>;
 
-    /** Subscription to remove changes in files. */
-    private _fileRemoveSubscription: Subscription | null;
+  /** Subscription to remove changes in files. */
+  private _fileRemoveSubscription: Subscription | null;
 
-    /** Subscription to changes in the files. */
-    private _changeSubscription: Subscription;
+  /** Subscription to changes in the files. */
+  private _changeSubscription: Subscription;
 
-    /** Combined stream of all of the file upload remove change events. */
-    get fileUploadRemoveEvents() {
-        return merge(...this.fileUploads.map(fileUpload => fileUpload.removeEvent));
-    }
+  /** Combined stream of all of the file upload remove change events. */
+  get fileUploadRemoveEvents() {
+    return merge(...this.fileUploads.map(fileUpload => fileUpload.removeEvent));
+  }
 
-    private files: Array<any> = [];
+  private files: Array<any> = [];
 
-    /* Http request input bindings */
-    @Input()
-    httpUrl: string;
+  /* Http request input bindings */
+  @Input()
+  httpUrl: string;
 
-    @Input()
-    httpRequestHeaders: HttpHeaders | {
-      [header: string]: string | string[];
-    } = new HttpHeaders();
+  @Input()
+  httpRequestHeaders: HttpHeaders | {
+    [header: string]: string | string[];
+  } = new HttpHeaders();
 
-    @Input()
-    httpRequestParams: HttpParams | {
-      [param: string]: string | string[];
-    } = new HttpParams();
+  @Input()
+  httpRequestParams: HttpParams | {
+    [param: string]: string | string[];
+  } = new HttpParams();
 
-    @Input()
-    fileAlias: string = "file";
+  @Input()
+  fileAlias: string = "file";
 
-    ngAfterViewInit() {
-      // When the list changes, re-subscribe
-      this._changeSubscription = this.fileUploads.changes.pipe(startWith(null)).subscribe(() => {
-        if (this._fileRemoveSubscription) {
-          this._fileRemoveSubscription.unsubscribe();
-        }
-        this._listenTofileRemoved();
-      });
-    }
-
-    private _listenTofileRemoved(): void {
-      this._fileRemoveSubscription = this.fileUploadRemoveEvents.subscribe((event: MatFileUpload) => {
-        this.files.splice(event.id, 1);
-      });
-    }
-
-    add(file: any) {
-      this.files.push(file);
-    }
-
-    public uploadAll() {
-      this.fileUploads.forEach((fileUpload) => { fileUpload.upload() });
-    }
-
-    public removeAll() {
-      this.files.splice(0, this.files.length);
-    }
-
-    ngOnDestroy() {
-      if (this.files) {
-        this.removeAll();
+  ngAfterViewInit() {
+    // When the list changes, re-subscribe
+    this._changeSubscription = this.fileUploads.changes.pipe(startWith(null)).subscribe(() => {
+      if (this._fileRemoveSubscription) {
+        this._fileRemoveSubscription.unsubscribe();
       }
+      this._listenTofileRemoved();
+    });
+  }
+
+  private _listenTofileRemoved(): void {
+    this._fileRemoveSubscription = this.fileUploadRemoveEvents.subscribe((event: MatFileUpload) => {
+      this.files.splice(event.id, 1);
+    });
+  }
+
+  add(file: any) {
+    this.files.push(file);
+  }
+
+  public uploadAll() {
+    this.fileUploads.forEach((fileUpload) => { fileUpload.upload() });
+  }
+
+  public removeAll() {
+    this.fileUploads.forEach((fileUpload) => { fileUpload.remove() });
+    this.files.splice(0, this.files.length);
+  }
+
+  ngOnDestroy() {
+    if (this.files) {
+      this.removeAll();
     }
+  }
 
 }
