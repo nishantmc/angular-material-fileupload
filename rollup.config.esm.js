b10ee44f73ff1731ae9cd744944165fe7e292e1f
Nishant <nishantmc@gmail.com>
2020-07-04 08:55:17 -0500

    Remove old files

--- rollup.config.esm.js
+++
@@ -1,4 +0,0 @@
-import config from './rollup.config.umd.js';
-config.output.format = "es";
-config.output.file = "dist/matFileUpload.esm.js";
-export default config;
--- src/lib/matFileUpload.ts
+++
@@ -1,5 +0,0 @@
-export * from './matFileUpload/matFileUpload.component';
-export * from './matFileUploadQueue/matFileUploadQueue.component';
-export * from './bytes.pipe';
-export * from './fileUploadInputFor.directive';
-export * from './matFileUpload.module';
