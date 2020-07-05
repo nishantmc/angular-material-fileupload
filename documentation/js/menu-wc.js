936caaf741c49019211d9dba3cc0f26da5745c4e
Nishant <nishantmc@gmail.com>
2020-07-05 11:00:51 -0500

    Add documentation

---
+++ documentation/js/menu-wc.js
@@ -0,0 +1,156 @@
+'use strict';
+
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">angular-material-fileupload documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter modules">
+                        <a data-type="chapter-link" href="modules.html">
+                            <div class="menu-toggler linked" data-toggle="collapse" ${ isNormalMode ?
+                                'data-target="#modules-links"' : 'data-target="#xs-modules-links"' }>
+                                <span class="icon ion-ios-archive"></span>
+                                <span class="link-name">Modules</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                        </a>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="modules-links"' : 'id="xs-modules-links"' }>
+                            <li class="link">
+                                <a href="modules/MatFileUploadModule.html" data-type="entity-link">MatFileUploadModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#components-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' : 'data-target="#xs-components-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' }>
+                                            <span class="icon ion-md-cog"></span>
+                                            <span>Components</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' :
+                                            'id="xs-components-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' }>
+                                            <li class="link">
+                                                <a href="components/MatFileUploadComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">MatFileUploadComponent</a>
+                                            </li>
+                                            <li class="link">
+                                                <a href="components/MatFileUploadQueueComponent.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">MatFileUploadQueueComponent</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                <li class="chapter inner">
+                                    <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                        'data-target="#directives-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' : 'data-target="#xs-directives-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' }>
+                                        <span class="icon ion-md-code-working"></span>
+                                        <span>Directives</span>
+                                        <span class="icon ion-ios-arrow-down"></span>
+                                    </div>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="directives-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' :
+                                        'id="xs-directives-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' }>
+                                        <li class="link">
+                                            <a href="directives/FileUploadInputForDirective.html"
+                                                data-type="entity-link" data-context="sub-entity" data-context-id="modules">FileUploadInputForDirective</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#pipes-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' : 'data-target="#xs-pipes-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' }>
+                                            <span class="icon ion-md-add"></span>
+                                            <span>Pipes</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="pipes-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' :
+                                            'id="xs-pipes-links-module-MatFileUploadModule-686a1f932936d33f315fa844750cf7fe"' }>
+                                            <li class="link">
+                                                <a href="pipes/BytesPipe.html"
+                                                    data-type="entity-link" data-context="sub-entity" data-context-id="modules">BytesPipe</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                            </li>
+                </ul>
+                </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#injectables-links"' :
+                                'data-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/MatFileUploadQueueService.html" data-type="entity-link">MatFileUploadQueueService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
+                            'data-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/IInput.html" data-type="entity-link">IInput</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/IUploadProgress.html" data-type="entity-link">IUploadProgress</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
